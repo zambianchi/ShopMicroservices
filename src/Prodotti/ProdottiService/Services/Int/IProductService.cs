497e11b209c0cf4c11b567a0853054fe3076bab5
Davide <davi98de@gmail.com>
2024-01-28 18:26:47 +0100

    Implementata interfaccia per get specific prodotti

--- src/Prodotti/ProdottiService/Services/Int/IProductService.cs
+++ src/Prodotti/ProdottiService/Services/Int/IProductService.cs
@@ -6,6 +6,7 @@ namespace ProdottiService.Services.Int
     {
         Task<List<ProductDTO>> GetProducts(CancellationToken cancellationToken);
         Task<ProductDTO> GetProduct(long idProduct, CancellationToken cancellationToken);
+        Task<List<ProductDTO>> GetSpecificProducts(List<long> idsProduct, CancellationToken cancellationToken);
         Task<ProductDTO> CreateProduct(CreateProductRequest request, CancellationToken cancellationToken);
         Task DeleteProduct(long idProduct, CancellationToken cancellationToken);
     }
