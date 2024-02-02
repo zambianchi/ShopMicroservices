5b6c6ba8c69c8b2c21ac4400f370bd5635a382b5
Davide <davi98de@gmail.com>
2024-02-02 23:59:21 +0100

    Aggiunte request per edit quantità disponibile prodotto

---
+++ src/Prodotti/ProdottiService/Models/API/Request/EditProductsAvailableAmountRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace ProdottiService.Models.API.Request
+{
+    public class EditProductsAvailableAmountRequest
+    {
+        public List<ProductAvailableForRequest> Products { get; set; }
+
+        public EditProductsAvailableAmountRequest(List<ProductAvailableForRequest> products)
+        {
+            this.Products = products;
+        }
+
+        public static EditProductsAvailableAmountRequest EditProductsAvailableAmountRequestFactory(List<ProductAvailableForRequest> products)
+        {
+            return new EditProductsAvailableAmountRequest(products);
+        }
+    }
+}
